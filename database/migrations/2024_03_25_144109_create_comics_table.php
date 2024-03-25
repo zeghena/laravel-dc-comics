92d873605cdcc57a6f603d4e3fbb81868591cb5f
davidemarchini <marchini73@gmail.com>
2024-03-25 16:01:06 +0100

    in comics_table modifico la migrazione appena creata per definire i campi del database necessari per il modello

--- database/migrations/2024_03_25_144109_create_comics_table.php
+++ database/migrations/2024_03_25_144109_create_comics_table.php
@@ -13,8 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('thumb');
+            $table->string('price');
+            $table->string('series');
+            $table->string('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
@@ -26,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comics');
     }
 };
